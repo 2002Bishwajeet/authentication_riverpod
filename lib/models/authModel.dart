e94484bb1110c9676d0296d17f58cf0d92019b6f
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-08-17 23:51:58 +0530

    Completed Authentication model

--- lib/models/authModel.dart
+++ lib/models/authModel.dart
@@ -1,1 +1,72 @@
-class Authentication {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Authentication {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream<User?> get authStateChange => _auth.authStateChanges();
+
+  //  SigIn the user using Email and Password
+  Future<void> signInWithEmailAndPassword(
+      String email, String password, BuildContext context) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      await showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text('Error Occured'),
+          content: Text(e.toString()),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+                child: Text("OK"))
+          ],
+        ),
+      );
+    }
+  }
+
+  //  SignIn the user Google
+  Future<void> signInWithGoogle(BuildContext context) async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    try {
+      await _auth.signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      await showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text('Error Occured'),
+          content: Text(e.toString()),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+                child: Text("OK"))
+          ],
+        ),
+      );
+    }
+  }
+
+  //  SignOut the current user
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
