6ef48919a501bd2d77a788784b065c4dbe048169
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-08-18 22:39:25 +0530

    added more comments

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -3,11 +3,27 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+//  Instead of creating Two Screens, I have Added both Login and Signup Screen in one Screen
+//  Yes , I am Lazy , But I am not going to create two screens , I am going to create one screen
+
+//  So for to monitor we are in which State we are i.e Login or signUp , I have used enums here
+//  So I have created and Enum Status which contains two things Login and SignUp
+
+//  and I have made a Global Variable type of Status, to use in LoginPage
+// It's actually not recommended to use Global Variables , but I am using it here to make it simple
+//  The main motive here was to teach Firebase Authentication using Riverpod as state management
+
 enum Status {
   login,
   signUp,
 }
-late Status type = Status.login;
+
+Status type = Status.login;
+
+//  I have used stateful widget to use setstate functions in LoginPage
+//  we could also managed the state using Riverpod but I am not using it here
+//  Remember Stateful widgets are made for a reason. If it would be bad
+//  flutter developer would not think of it in the first place.
 
 class LoginPage extends StatefulWidget {
   static const routename = '/LoginPage';
@@ -18,9 +34,16 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //  GlobalKey is used to validate the Form
   final GlobalKey<FormState> _formKey = GlobalKey();
+
+  //  TextEditingController to get the data from the TextFields
+  //  we can also use Riverpod to manage the state of the TextFields
+  //  but again I am not using it here
   final _email = TextEditingController();
   final _password = TextEditingController();
+
+  //  A loading variable to show the loading animation when you a function is ongoing
   bool _isLoading = false;
   void loading() {
     setState(() {
@@ -196,6 +219,9 @@ class _LoginPageState extends State<LoginPage> {
                                   child: Row(
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
+                                      //  A google icon here
+                                      //  an External Package used here
+                                      //  Font_awesome_flutter package used
                                       FaIcon(FontAwesomeIcons.google),
                                       Text(
                                         ' Login with Google',
