7d7a56721d9a1a5c06c77df68980dc2a12264144
2002Bishwajeet <bishwajeet.techmaster@gmail.com>
2022-07-05 23:54:58 +0530

    misc fixes

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -32,7 +32,7 @@ class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
